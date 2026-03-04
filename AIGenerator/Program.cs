54b919ffdf0ba95e8dee26f0b280ddf6a8d01b49
rymdje <rym.djerourou@gmail.com>
2026-03-04 16:06:10 +0100

    Add project files.

---
+++ AIGenerator/Program.cs
@@ -0,0 +1,68 @@
+﻿class Program
+{
+
+    private static string _filePath = @"ADEME-CarLabelling.csv";
+    private static string _saveFilePath = @"ADEME-CarLabelling-Clean.csv";
+
+    static void Main(string[] args)
+    {
+
+        try
+        {
+            var fichier = File.ReadAllLines(_filePath);
+            var header = fichier[0].Split(';');
+
+            int indexPuissance = Array.IndexOf(header, "\"Puissance maximale\"");
+            int indexElec = Array.IndexOf(header, "\"Puissance nominale électrique\"");
+            int indexPoidsVide = Array.IndexOf(header, "\"Poids à vide\"");
+            int indexConsoVitMixtMin = Array.IndexOf(header, "\"Conso vitesse mixte Min\"");
+            int indexCO2VitMixteMin = Array.IndexOf(header, "\"CO2 vitesse mixte Min\"");
+            int indexEnergie = Array.IndexOf(header, "Energie");
+
+            var output = new List<string>();
+            output.Add("Puissance;PoidsAVide;ConsoMixte;Energie;CO2");
+
+            foreach (var item in fichier.Skip(1))
+            {
+                var cols = item.Split(';');
+
+                var puissance = cols[indexPuissance];
+                var elec = cols[indexElec];
+
+                if(string.IsNullOrWhiteSpace(puissance) )
+                {
+                    puissance = elec;
+                }
+
+                var poidsVide = cols[indexPoidsVide].Replace(",", ".");
+                var consoMixte = cols[indexConsoVitMixtMin].Replace(",", ".");
+                var energie = cols[indexEnergie];
+                puissance = puissance.Replace(",", ".");
+                var co2Mixte = cols[indexCO2VitMixteMin].Replace(",", ".");
+
+                if (string.IsNullOrWhiteSpace(co2Mixte))
+                {
+                    co2Mixte = "0";
+                }
+
+
+
+                if (!(string.IsNullOrWhiteSpace(puissance)
+                    && string.IsNullOrWhiteSpace(poidsVide)
+                    && string.IsNullOrWhiteSpace(consoMixte)
+                    && string.IsNullOrWhiteSpace(co2Mixte)
+                    && string.IsNullOrWhiteSpace(energie)))
+                {
+                    output.Add($"{puissance};{poidsVide};{consoMixte};{energie};{co2Mixte}");
+                    
+                }
+
+            }
+            File.WriteAllLines(_saveFilePath, output);
+
+        } catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
---
+++ TDTP1_1_CSVHelper/Program.cs
@@ -0,0 +1,74 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace TDTP1_1_CSVHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture);
+            config.Delimiter = ";";
+            config.ReadingExceptionOccurred = (ex) => { return false; };
+
+            using var reader = new StreamReader("vehicules.csv");
+            using var csvReader = new CsvReader(reader, config);
+            csvReader.Context.RegisterClassMap<CarDataMap>();
+
+            var vehicules = csvReader.GetRecords<CarData>();
+
+            using var writer = new StreamWriter("vehicules_output.csv");
+            using var csvWriter = new CsvWriter(writer, config);
+            csvWriter.WriteHeader<CarData>();
+            csvWriter.NextRecord();
+            foreach (var car in vehicules)
+            {
+                csvWriter.WriteRecord<CarData>(car);
+                csvWriter.NextRecord();
+            }
+            Console.WriteLine("Fin de nettoyage");
+        }
+
+        public class CarData
+        {
+            public float Puissance { get; set; }
+            public float PoidsAVide { get; set; }
+            public float ConsoMixte { get; set; }
+            public float CO2 { get; set; }
+            public string Energie { get; set; }
+        }
+
+        public class CarDataMap : ClassMap<CarData>
+        {
+            public CarDataMap()
+            {
+
+                Map(m => m.Puissance).Convert(row =>
+                {
+                    var pMax = row.Row.GetField("Puissance maximale");
+                    var pElec = row.Row.GetField("Puissance nominale électrique");
+                    var puissance = !string.IsNullOrWhiteSpace(pMax) ? pMax : pElec;
+                    return float.Parse(puissance.Replace(',', '.'), CultureInfo.InvariantCulture);
+                });
+                Map(m => m.PoidsAVide).Name("Poids à vide").Convert(row =>
+                {
+                    var poids = row.Row.GetField("Poids à vide");
+                    return float.Parse(poids.Replace(',', '.'), CultureInfo.InvariantCulture);
+                });
+                Map(m => m.ConsoMixte).Name("Conso vitesse mixte Min").Convert(row =>
+                {
+                    var conso = row.Row.GetField("Conso vitesse mixte Min");
+                    return float.Parse(conso.Replace(',', '.'), CultureInfo.InvariantCulture);
+                });
+                Map(m => m.CO2).Name("CO2 vitesse mixte Min").Convert(row =>
+                {
+                    var co2 = row.Row.GetField("CO2 vitesse mixte Min");
+                    return float.Parse(co2.Replace(',', '.'), CultureInfo.InvariantCulture);
+                });
+                Map(m => m.Energie).Name("Energie");
+            }
+        }
+    }
+}
